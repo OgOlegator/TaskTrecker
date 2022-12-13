7aceb24b08006761ee6e4fa28bc76ffd9d8f5bb0
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2022-12-13 23:22:58 +0300

    Доделан контроллер TasksProject Добавлены комментарии к репозиториям

--- TaskTrecker.TaskTreckerApi/Controllers/TasksProjectController.cs
+++ TaskTrecker.TaskTreckerApi/Controllers/TasksProjectController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TaskTrecker.TaskTreckerApi.Models.Dto;
+using TaskTrecker.TaskTreckerApi.Repository.IRepository;
 
 namespace TaskTrecker.TaskTreckerApi.Controllers
 {
@@ -7,8 +9,35 @@ namespace TaskTrecker.TaskTreckerApi.Controllers
     [ApiController]
     public class TasksProjectController : ControllerBase
     {
+        private readonly ITaskRepository _repository;
+        private ResponseDto _response = new ResponseDto();
 
+        public TasksProjectController(ITaskRepository repository)
+        {
+            _repository = repository;
+        }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="projectId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{projectId}")]
+        public async Task<ResponseDto> GetProjectTasks(string projectId)
+        {
+            try
+            {
+                _response.Result = await _repository.GetTasksByProject(int.Parse(projectId));
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.DisplayMessage = ex.Message;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            
+            return _response;
+        }
     }
 }
--- TaskTrecker.TaskTreckerApi/Repository/IRepository/ITaskRepository.cs
+++ TaskTrecker.TaskTreckerApi/Repository/IRepository/ITaskRepository.cs
@@ -5,21 +5,59 @@ namespace TaskTrecker.TaskTreckerApi.Repository.IRepository
 {
     public interface ITaskRepository
     {
-
+        /// <summary>
+        /// Add new task or change task info in data base
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
         Task<Models.Task> CreateUpdateTask(Models.Task task);
 
+        /// <summary>
+        /// Delete task in data base
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<bool> DeleteTask(int id);
 
+        /// <summary>
+        /// Get all tasks from data base
+        /// </summary>
+        /// <returns></returns>
         Task<IEnumerable<Models.Task>> GetTasks();
 
+        /// <summary>
+        /// Get project tasks
+        /// </summary>
+        /// <param name="projectId"></param>
+        /// <returns></returns>
         Task<IEnumerable<Models.Task>> GetTasksByProject(int projectId);
 
+        /// <summary>
+        /// Get concrete task in data base
+        /// </summary>
+        /// <param name="taskId"></param>
+        /// <returns></returns>
         Task<Models.Task> GetTaskById(int taskId);
 
+        /// <summary>
+        /// Get tasks by the occurrence of the search string
+        /// </summary>
+        /// <param name="nameTask"></param>
+        /// <returns></returns>
         Task<IEnumerable<Models.Task>> GetTasksByName(string nameTask);
 
+        /// <summary>
+        /// Get tasks by execution status
+        /// </summary>
+        /// <param name="status"></param>
+        /// <returns></returns>
         Task<IEnumerable<Models.Task>> GetTasksByStatus(StatusTask status);
 
+        /// <summary>
+        /// Get tasks by task execution priority
+        /// </summary>
+        /// <param name="priority"></param>
+        /// <returns></returns>
         Task<IEnumerable<Models.Task>> GetTasksByPriority(Priority priority);
 
     }
