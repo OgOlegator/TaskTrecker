a49d4616dc1f5bde14a43652c3a59fe55f31c1ef
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2022-12-16 13:29:00 +0300

    Добавлены модели dto

---
+++ TaskTrecker.TaskTreckerApi/Models/Dto/ProjectDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskTrecker.TaskTreckerApi.Models.Dto
+{
+    public class ProjectDto
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public StatusProject Status { get; set; }
+
+        public SD.Priority Priority { get; set; }
+
+    }
+}
---
+++ TaskTrecker.TaskTreckerApi/Models/Dto/TaskDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskTrecker.TaskTreckerApi.Models.Dto
+{
+    public class TaskDto
+    {
+
+        public int Id { get; set; }
+
+        public int IdProject { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public StatusTask Status { get; set; }
+
+        public SD.Priority Priority { get; set; }
+
+    }
+}
